ca4e9447d5a3d42d14fd8437c69f901410b69009
priyankareddyvadicherla@gmail.com <priyankareddyvadicherla@gmail.com>
2022-05-05 11:17:08 +0000

    tags added

---
+++ models/staging/jaffle_shop/stg_cust.sql
@@ -0,0 +1,15 @@
+{{ config(
+    tags = ["finance"]
+) }}
+
+with customers as (
+    
+    select 
+        id as customer_id,
+        first_name,
+        last_name
+
+    from {{ source('jaffle_shop', 'customers') }}
+)
+
+select * from customers
